f0c937d721c10d5751d8ae9f2b211c447810c77a
Emily Hunt <emily.hunt.physics@gmail.com>
2021-10-14 14:41:52 +0200

    New ways to sample King profiles

--- ocelot/calculate/profile.py
+++ ocelot/calculate/profile.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Optional
 
 import numpy as np
 
@@ -94,8 +94,82 @@ def king_surface_density_fast(r_values: np.ndarray, r_core: float, r_tidal: floa
     return ((1 + (r_values / r_core) ** 2) ** (-0.5) - a) ** 2
 
 
-def sample_king_profile(r_core: float, r_tidal: float, n_samples: int):
-    """Samples a 2D King profile to return n_samples sample radii."""
-    # Fuck I realise sampling the 2D profile isn't actually useful for why I'm writing this code right now, so I'll
-    # leave it for now lol (I need to sample just 1D distances from the core...)
-    pass
+def sample_2d_king_profile(r_core: float, r_tidal: float, n_samples: int, seed=None, oversampling_factor: float = 10,
+                           return_generator: bool = False):
+    """Samples a 2D King profile to return n_samples sample radii.
+
+    Valid only for:
+        0 < r_core < r_tidal
+
+    Args:
+        r_core (float): the core radius of the cluster.
+        r_tidal (float): the tidal radius of the cluster.
+        n_samples (int): the number of samples to generate.
+        seed (int, optional): the seed of the random number generator. Default: None.
+        oversampling_factor (float): how many times n_samples to generate each step, which helps to make sure that
+            enough samples are quickly generated in just one or two loops. Default: 10.
+        return_generator (bool): whether or not to return the numpy random number generator created. Default: False
+
+    Returns:
+        an array of sample radii of size n_samples, plus the random generator if return_generator==True.
+    """
+    _check_core_and_tidal_radii(r_core, r_tidal)
+
+    r_samples = np.empty(n_samples, dtype=float)
+    generator = np.random.default_rng(seed=seed)
+    completed_samples = 0
+
+    while completed_samples < n_samples:
+        remaining_samples = n_samples - completed_samples
+        samples_to_generate = int(np.clip(remaining_samples * oversampling_factor, 10, np.inf))
+
+        # Generate some initial radius samples
+        test_r_values = generator.uniform(high=r_tidal, size=samples_to_generate)
+        test_king_values = king_surface_density_fast(test_r_values, r_core, r_tidal)
+        test_mcmc_values = generator.uniform(size=samples_to_generate)
+
+        # See which & how many are valid and save them!
+        valid_test_samples = test_king_values < test_mcmc_values
+        n_valid_test_samples = np.count_nonzero(valid_test_samples)
+
+        if n_valid_test_samples > remaining_samples:
+            r_samples[completed_samples:] = test_r_values[valid_test_samples][:remaining_samples]
+        else:
+            r_samples[completed_samples:completed_samples + n_valid_test_samples] = test_r_values[valid_test_samples]
+
+    if return_generator:
+        return r_samples, generator
+    else:
+        return r_samples
+
+
+def sample_1d_king_profile(r_core: float, r_tidal: float, n_samples: int, seed: Optional[int] = None,
+                           oversampling_factor: float = 10):
+    """Samples a 1D King profile (e.g. useful to get line of sight distances from the center of a cluster.) Uses a
+    little trick - assumes that we're looking at the cluster side-on and removes the not-line-of-sight axis as if we
+    were looking at it from the front. (This is because I cba to work out a 1D profile and more to the point, I couldn't
+    fucking find one)
+
+    Todo: change this to using the strip density g(x), which I *think* could do this better...
+
+    Valid only for:
+        0 < r_core < r_tidal
+
+    Args:
+        r_core (float): the core radius of the cluster.
+        r_tidal (float): the tidal radius of the cluster.
+        n_samples (int): the number of samples to generate.
+        seed (int, optional): the seed of the random number generator. Default: None.
+        oversampling_factor (float): how many times n_samples to generate each step, which helps to make sure that
+            enough samples are quickly generated in just one or two loops. Default: 10.
+
+    Returns:
+        an array of sample radii of size n_samples
+    """
+    r_samples, generator = sample_2d_king_profile(r_core, r_tidal, n_samples, seed=seed,
+                                                  oversampling_factor=oversampling_factor, return_generator=True)
+
+    # Now, deproject this to 1D by giving every value an angle and then finding the 1D radius with the cosine
+    random_angles = generator.uniform(high=2 * np.pi, size=n_samples)
+
+    return r_samples * np.cos(random_angles)
