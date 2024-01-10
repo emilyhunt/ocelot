8df990f1356fe17c4d115f7568f512ab7a50d526
emilyhunt <emily.hunt.physics@gmail.com>
2024-01-10 16:28:41 +0100

    Remove some useless stuff + more dep warnings

--- src/ocelot/crossmatch/__init__.py
+++ src/ocelot/crossmatch/__init__.py
@@ -1,1 +1,7 @@
 from ._catalogue import Catalogue
+import warnings
+warnings.warn(
+    "ocelot.crossmatch API will change soon. Most objects will move, change, or be "
+    "deprecated.",
+    DeprecationWarning,
+)
--- src/ocelot/plot/__init__.py
+++ src/ocelot/plot/__init__.py
@@ -6,3 +6,9 @@ from .plot_figure import isochrone
 from .plot_figure import location
 from .plot_figure import nearest_neighbor_distances
 from .gaia_explorer import GaiaExplorer, ion, ioff
+import warnings
+warnings.warn(
+    "ocelot.plot API will change soon. Most objects will move, change, or be "
+    "deprecated.",
+    DeprecationWarning,
+)
