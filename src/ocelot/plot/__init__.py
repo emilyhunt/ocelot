338ec36b7f256719db4ad71f9c8e6997cf188cf2
Emily Hunt <emily.hunt.physics@gmail.com>
2024-02-26 14:22:42 +0100

    yet more api removals

--- src/ocelot/plot/__init__.py
+++ src/ocelot/plot/__init__.py
@@ -2,7 +2,6 @@ from . import axis
 from . import process
 from . import utilities
 from .plot_figure import clustering_result
-from .plot_figure import isochrone
 from .plot_figure import location
 from .plot_figure import nearest_neighbor_distances
 from .gaia_explorer import GaiaExplorer, ion, ioff
