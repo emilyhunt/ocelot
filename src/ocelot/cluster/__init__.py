df5c6ba8399a82c1ade0a898bc18097d22374764
Emily Hunt <emily.hunt.physics@gmail.com>
2024-02-26 10:10:08 +0100

    Remove more deleted imports

--- src/ocelot/cluster/__init__.py
+++ src/ocelot/cluster/__init__.py
@@ -1,9 +1,7 @@
-from . import epsilon, synthetic
+from . import epsilon
 from .nearest_neighbor import precalculate_nn_distances
 from .preprocess import cut_dataset, rescale_dataset, recenter_dataset
-from .synthetic import SimulatedPopulations, generate_synthetic_clusters
 from .resample import generate_gaia_covariance_matrix, resample_gaia_astrometry
-from .partition import DataPartition
 import warnings
 
 warnings.warn(
