f7ee1a6cda10c720a66fcee3ccd4cc964638152a
emilyhunt <emily.hunt.physics@gmail.com>
2024-11-25 16:35:09 +0100

    Remove unneeded path

--- src/ocelot/__init__.py
+++ src/ocelot/__init__.py
@@ -7,7 +7,7 @@ __version__ = "0.4.0"
 # Set a library path, helping other modules to find the data directory etc.
 MODULE_PATH = Path(importlib.util.find_spec("ocelot").origin).parent
 
-# Set a data path
+# Set a data path where large files are downloaded to
 DATA_PATH = MODULE_PATH.parent.parent / "data"
 if environment_path := os.getenv("OCELOT_DATA"):
     DATA_PATH = Path(environment_path)
