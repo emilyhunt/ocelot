40b63ca97704554b1e5f71e22e29d7ddfac18e40
emilyhunt <emily.hunt.physics@gmail.com>
2025-01-10 16:37:12 +0100

    Move things around & add a new random unit vector function

--- src/ocelot/util/coordinates.py
+++ src/ocelot/util/coordinates.py
@@ -1,4 +1,4 @@
-"""Common functions to help with transforms and sampling."""
+"""Functions to help with common coordinate problems."""
 
 import numpy as np
 
@@ -9,6 +9,7 @@ def spherical_to_cartesian(radii, thetas, phis):
 
     Assumes radii ∈ [0, inf), thetas ∈ [0, pi], phis ∈ [0, 2pi)
     """
+    # Todo refactor to a more useful place
     x_values = radii * np.sin(thetas) * np.cos(phis)
     y_values = radii * np.sin(thetas) * np.sin(phis)
     z_values = radii * np.cos(thetas)
